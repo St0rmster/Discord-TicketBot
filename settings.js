66c4a7ebe30a4331c104976c64ddd90e136a4ac6
St0rmster <bernardwind6@gmail.com>
2023-02-10 16:22:48 +1300

    DT Trading Discord Ticket Bot
    I have been Sitting on this project for awhile but only knew how to upload things onto github with GitLFS and only figured out how to do this now.

---
+++ settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+    token: "BOT_TOKEN",
+    ticketChannel: "TICKET_CHANNEL",
+    ticketRoles: ["TICKET_MANAGER_ROLE"],
+    ticketCategory: "TICKET_CATEGORY",
+    guildId: "GUILD_ID",
+  };
+  
